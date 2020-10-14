7e9daec558aed13fdb649f4bf8e4e9ece8f16fbf
agusventuri <PCw10SPl502>
2020-10-14 19:16:53 -0300

    Refactoring data providing

---
+++ flicking/app/src/main/java/com/baccaventuri/flicking/Data/DataProvider.java
@@ -0,0 +1,10 @@
+package com.baccaventuri.flicking.Data;
+
+import com.baccaventuri.flicking.Models.Photoset;
+
+public class DataProvider {
+
+    public Photoset loadPhotoset (String id) {
+        return null;
+    }
+}
